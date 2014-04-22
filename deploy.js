cfa424c29f0276b47b3cfb64a97e48b06522d27e
Nathan Rijksen <n.rijksen@gmail.com>
2014-04-22 16:38:50 -0400

    added deploy script

---
+++ deploy.js
@@ -0,0 +1,77 @@
+(function() {
+
+    var logger = console;
+
+    this.init = function(customLogger)
+    {
+        logger = customLogger;
+    }
+
+    this.run = function(params, done)
+    {
+        logger.info("======================================================================");
+        logger.info("Running job " + __dirname);
+        logger.info("======================================================================");
+        logger.debug(params);
+
+        var exec = require('child_process').exec;
+        var commands = [
+            "npm prune",
+            "npm install",
+            "bower prune",
+            "bower install",
+            "docpad clean"
+        ];
+
+        // Deploy to github pages if this is the production branch, otherwise
+        // just compile it locally
+        if (params.branch == "production")
+        {
+            commands.push("docpad deploy-ghpages --env static --silent");
+        }
+        else
+        {
+            commands.push("docpad generate --silent");
+        }
+
+        var runCommand = function(index)
+        {
+            if ( ! (index in commands)) return done();
+
+            var command = commands[index];
+            logger.info("Executing " + command);
+            exec('cd "' + __dirname + '" && ' + command, function(err, stdo, stde)
+            {
+                if (err) return done(err);
+                logger.debug("Result: " + stdo + stde + "\n");
+                runCommand(++index);
+            });
+        }
+
+        runCommand(0);
+    };
+    
+    this.schedule = function(branch, cron, deploy)
+    {
+        logger.info("Scheduling deployment for " + __dirname + " : " + branch);
+
+        var doDeploy = function()
+        {
+            deploy({
+                name: 'komodo-website-' + branch,
+                path: __dirname,
+                repository: 'komodo-website',
+                branch: branch
+            });
+        };
+
+        // Deploy latest version every 3 hours
+        new cron('0 */3 * * *', function(){
+            logger.info('Running scheduled deploy for ' + __dirname);
+            doDeploy();
+        }, null, true);
+
+        doDeploy();
+    };
+
+}).apply(module.exports);
