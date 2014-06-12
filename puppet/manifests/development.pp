c65becde7cadc19ae2256e1b4d984169dfae1105
cgchoffman <cgchoffman@gmail.com>
2014-06-11 17:13:04 -0700

    Attempting to get exec Puppet commands to run successfully

--- puppet/manifests/development.pp
+++ puppet/manifests/development.pp
@@ -1,11 +1,36 @@
 class install-npm-packages{
-  $npmPackages = ['docpad', 'bower', 'grunt']
+  $npmPackages = ['bower']
   package{ $npmPackages:
     ensure => 'installed',
     provider => 'npm',
-    require => Class['nodejs'],
+    before => Class['ko-bower-install'],
+    require => Class['nodejs']
+  }
+}
+
+$nodepath = ["/usr/local/node/node-default/bin/", "/usr/local/bin"]
+
+class ko-npm-install{
+  exec{ "npm install":
+    command => "npm install",
+    creates => "/vagrant/node_modules",
+    cwd => "/vagrant",
+    path => $nodepath,
+    require => Class['nodejs']
+  }
+}
+
+class ko-bower-install{
+  exec{ "bower install":
+    command => "bower install",
+    creates => "/vagrant",
+    cwd => "/vagrant",
+    path => $nodepath,
+    require => Class["install-npm-packages"]
   }
 }
 include git
 include nodejs
 include install-npm-packages
+include ko-npm-install
+include ko-bower-install
