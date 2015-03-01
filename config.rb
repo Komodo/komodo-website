13d847bc3f3cadda5065f22886c0eeb8cfbef718
Nathan Rijksen <n.rijksen@gmail.com>
2015-03-01 16:51:00 -0500

    Cleanup config noise

--- config.rb
+++ config.rb
@@ -1,42 +1,5 @@
-###
-# Compass
-###
-
-# Change Compass configuration
-# compass_config do |config|
-#   config.output_style = :compact
-# end
-
-###
-# Page options, layouts, aliases and proxies
-###
-
-# Per-page layout changes:
-#
-# With no layout
-# page "/path/to/file.html", :layout => false
-#
-# With alternative layout
-# page "/path/to/file.html", :layout => :otherlayout
-#
-# A path which all have the same layout
-# with_layout :admin do
-#   page "/admin/*"
-# end
-
-# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
-#  :which_fake_page => "Rendering a fake page with a local variable" }
-
-###
-# Helpers
-###
-
 require 'helpers'
 helpers Helpers
-
-# Automatic image dimensions on image_tag helper
-# activate :automatic_image_sizes
 
 activate :blog do |blog|
   blog.prefix = "blog"
@@ -47,18 +10,10 @@ end
 
 activate :directory_indexes
 
-# Reload the browser automatically whenever files change
 configure :development do
   activate :livereload
   set :site_url, "http://dev.komodoide.com:4567/"
 end
-
-# Methods defined in the helpers block are available in templates
-# helpers do
-#   def some_helper
-#     "Helping"
-#   end
-# end
 
 set :css_dir, 'assets/stylesheets'
 set :js_dir, 'assets/javascripts'
